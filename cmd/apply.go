a981ed0e3696336f06c361f0d558f144b987d151
Luc DUZAN <lduzan@conduktor.io>
2024-02-29 17:23:01 +0100

    dry mode

--- cmd/apply.go
+++ cmd/apply.go
@@ -9,6 +9,7 @@ import (
 )
 
 var filePath *[]string
+var dryRun *bool
 
 // applyCmd represents the apply command
 var applyCmd = &cobra.Command{
@@ -16,7 +17,7 @@ var applyCmd = &cobra.Command{
 	Short: "upsert a resource on Conduktor",
 	Long:  ``,
 	Run: func(cmd *cobra.Command, args []string) {
-		var resources []resource.Resource = make([]resource.Resource, 0)
+		var resources = make([]resource.Resource, 0)
 		for _, path := range *filePath {
 			r, err := resourceForPath(path)
 			if err != nil {
@@ -27,7 +28,7 @@ var applyCmd = &cobra.Command{
 		}
 		client := client.MakeFromEnv(*debug)
 		for _, resource := range resources {
-			upsertResult, err := client.Apply(&resource)
+			upsertResult, err := client.Apply(&resource, *dryRun)
 			if err != nil {
 				fmt.Fprintf(os.Stderr, "Could not apply resource %s/%s: %s\n", resource.Kind, resource.Name, err)
 				os.Exit(1)
@@ -57,6 +58,9 @@ func init() {
 	// Here you will define your flags and configuration settings.
 	filePath = applyCmd.
 		PersistentFlags().StringArrayP("file", "f", make([]string, 0, 0), "Specify the files to apply")
+
+	dryRun = applyCmd.
+		PersistentFlags().Bool("dry-mode", false, "Don't really apply change but check on backend the effect if applied")
 
 	applyCmd.MarkPersistentFlagRequired("file")
 }
