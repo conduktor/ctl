6d7bb5930ee9352d57429adc88729511a63f776c
Luc DUZAN <lduzan@conduktor.io>
2024-06-25 14:34:08 +0200

    use order from default catalog if catalog don't have any

--- schema/sort.go
+++ schema/sort.go
@@ -9,11 +9,11 @@ import (
 	"github.com/conduktor/ctl/resource"
 )
 
-func resourcePriority(catalog KindCatalog, resource resource.Resource, debug bool) int {
+func resourcePriority(catalog KindCatalog, resource resource.Resource, debug, failbackToDefaultCatalog bool) int {
 	kind, ok := catalog[resource.Kind]
 	if !ok {
 		if debug {
-			fmt.Fprintf(os.Stderr, "Could not find kind: %s in catalog, default to DefaultPriority for resource ordering", resource.Kind)
+			fmt.Fprintf(os.Stderr, "Could not find kind: %s in catalog, default to DefaultPriority for resource ordering\n", resource.Kind)
 		}
 		return DefaultPriority
 	}
@@ -21,17 +21,27 @@ func resourcePriority(catalog KindCatalog, resource resource.Resource, debug boo
 	kindVersion, ok := kind.Versions[version]
 	if !ok {
 		if debug {
-			fmt.Fprintf(os.Stderr, "Could not find version: %d of kind %s in catalog, default to DefaultPriority for resource ordering", version, resource.Kind)
+			fmt.Fprintf(os.Stderr, "Could not find version: %d of kind %s in catalog, default to DefaultPriority for resource ordering\n", version, resource.Kind)
 		}
 		return DefaultPriority
 	} else {
-		return kindVersion.Order
+		order := kindVersion.Order
+		if order == DefaultPriority && failbackToDefaultCatalog {
+			defaultCatalog := DefaultKind()
+			orderFromDefaultCatalog := resourcePriority(defaultCatalog, resource, false, false)
+			if orderFromDefaultCatalog != DefaultPriority && debug {
+				fmt.Fprintf(os.Stderr, "Could not find version: %d of kind %s in catalog, but find it in default catalog with priority %d\n", version, resource.Kind, orderFromDefaultCatalog)
+			}
+			return orderFromDefaultCatalog
+		} else {
+			return kindVersion.Order
+		}
 	}
 }
 
 func SortResources(catalog KindCatalog, resources []resource.Resource, debug bool) {
 	sort.SliceStable(resources, func(i, j int) bool {
-		return resourcePriority(catalog, resources[i], debug) < resourcePriority(catalog, resources[j], debug)
+		return resourcePriority(catalog, resources[i], debug, true) < resourcePriority(catalog, resources[j], debug, true)
 	})
 
 }
