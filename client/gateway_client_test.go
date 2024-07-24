9b216098a7dc4bb258f0e19bcb74498121961432
achever1 <achever@conduktor.io>
2024-07-24 15:11:54 +0200

    set it for gw

--- client/gateway_client_test.go
+++ client/gateway_client_test.go
@@ -8,7 +8,7 @@ import (
 	"github.com/jarcoal/httpmock"
 )
 
-var aGwResource = resource.Resource{
+var aVClusterResource = resource.Resource{
 	Version:  "gateway/v2",
 	Kind:     "VClusters",
 	Name:     "vcluster1",
@@ -16,21 +16,6 @@ var aGwResource = resource.Resource{
 	Json:     []byte(`{"apiVersion":"v1","kind":"VClusters","metadata":{"name":"vcluster1"},"spec":{"prefix":"vcluster1_"}}`),
 }
 
-func TestUniformizeGwBaseUrl(t *testing.T) {
-	validUrls := []string{
-		"http://baseUrl/api/",
-		"http://baseUrl/api",
-		"http://baseUrl/",
-		"http://baseUrl",
-	}
-	expectedResult := "http://baseUrl/api"
-	for _, url := range validUrls {
-		finalUrl := uniformizeBaseUrl(url)
-		if finalUrl != expectedResult {
-			t.Errorf("When uniformize %s got %s expected %s", url, finalUrl, expectedResult)
-		}
-	}
-}
 func TestGwApplyShouldWork(t *testing.T) {
 	defer httpmock.Reset()
 	baseUrl := "http://baseUrl"
@@ -48,27 +33,17 @@ func TestGwApplyShouldWork(t *testing.T) {
 	)
 	responder := httpmock.NewStringResponder(200, `{"upsertResult": "NotChanged"}`)
 
-	vCluster := resource.Resource{
-		Json:    []byte(`{"": "data"}`),
-		Kind:    "VClusters",
-		Name:    "vcluster1",
-		Version: "gateway/v2",
-		Metadata: map[string]interface{}{
-			"name": "titi",
-		},
-	}
-
 	httpmock.RegisterMatcherResponderWithQuery(
 		"PUT",
 		"http://baseUrl/gateway/v2/vclusters",
 		nil,
 		httpmock.HeaderIs("Authorization", "Basic YWRtaW46Y29uZHVrdG9y").
 			And(httpmock.HeaderIs("X-CDK-CLIENT", "CLI/unknown")).
-			And(httpmock.BodyContainsBytes(vCluster.Json)),
+			And(httpmock.BodyContainsBytes(aVClusterResource.Json)),
 		responder,
 	)
 
-	body, err := gatewayClient.Apply(&vCluster, false)
+	body, err := gatewayClient.Apply(&aVClusterResource, false)
 	if err != nil {
 		t.Error(err)
 	}
@@ -94,23 +69,16 @@ func TestGwApplyWithDryModeShouldWork(t *testing.T) {
 	)
 	responder := httpmock.NewStringResponder(200, `{"upsertResult": "NotChanged"}`)
 
-	vCluster := resource.Resource{
-		Json:    []byte(`{"yolo": "data"}`),
-		Kind:    "VClusters",
-		Name:    "toto",
-		Version: "gateway/v2",
-	}
-
 	httpmock.RegisterMatcherResponderWithQuery(
 		"PUT",
 		"http://baseUrl/gateway/v2/vclusters",
 		"dryMode=true",
 		httpmock.HeaderIs("Authorization", "Basic YWRtaW46Y29uZHVrdG9y").
-			And(httpmock.BodyContainsBytes(vCluster.Json)),
+			And(httpmock.BodyContainsBytes(aVClusterResource.Json)),
 		responder,
 	)
 
-	body, err := gatewayClient.Apply(&vCluster, true)
+	body, err := gatewayClient.Apply(&aVClusterResource, true)
 	if err != nil {
 		t.Error(err)
 	}
@@ -140,23 +108,16 @@ func TestGwApplyShouldFailIfNo2xx(t *testing.T) {
 		panic(err)
 	}
 
-	vCluster := resource.Resource{
-		Json:    []byte(`{"yolo": "data"}`),
-		Kind:    "Application",
-		Name:    "toto",
-		Version: "v1",
-	}
-
 	httpmock.RegisterMatcherResponderWithQuery(
 		"PUT",
 		"http://baseUrl/gateway/v2/vclusters",
 		nil,
 		httpmock.HeaderIs("Authorization", "Basic YWRtaW46Y29uZHVrdG9y").
-			And(httpmock.BodyContainsBytes(vCluster.Json)),
+			And(httpmock.BodyContainsBytes(aVClusterResource.Json)),
 		responder,
 	)
 
-	_, err = gatewayClient.Apply(&vCluster, false)
+	_, err = gatewayClient.Apply(&aVClusterResource, false)
 	if err == nil {
 		t.Failed()
 	}
@@ -177,7 +138,7 @@ func TestGwGetShouldWork(t *testing.T) {
 	httpmock.ActivateNonDefault(
 		gatewayClient.client.GetClient(),
 	)
-	responder, err := httpmock.NewJsonResponder(200, []resource.Resource{aGwResource})
+	responder, err := httpmock.NewJsonResponder(200, []resource.Resource{aVClusterResource})
 	if err != nil {
 		panic(err)
 	}
@@ -191,13 +152,13 @@ func TestGwGetShouldWork(t *testing.T) {
 		responder,
 	)
 
-	vCluster := gatewayClient.GetKinds()["VClusters"]
-	result, err := gatewayClient.Get(&vCluster, []string{})
+	vClusterKind := gatewayClient.GetKinds()["VClusters"]
+	result, err := gatewayClient.Get(&vClusterKind, []string{})
 	if err != nil {
 		t.Error(err)
 	}
-	if !reflect.DeepEqual(result[0].Json, aGwResource.Json) {
-		t.Errorf("Bad result expected %v got: %v", aGwResource, result)
+	if !reflect.DeepEqual(result[0].Json, aVClusterResource.Json) {
+		t.Errorf("Bad result expected %v got: %v", aVClusterResource, result)
 	}
 }
 
--- test_final_exec.sh
+++ test_final_exec.sh
@@ -13,21 +13,26 @@ trap cleanup EXIT
 main() {
 	cd "$SCRIPTDIR"
 	docker compose -f docker/docker-compose.yml build
-	docker compose -f docker/docker-compose.yml up -d mock
+	docker compose -f docker/docker-compose.yml up -d mock mockGateway
 	sleep 1
-	docker compose -f docker/docker-compose.yml run conduktor apply -f /test_resource.yml
-	docker compose -f docker/docker-compose.yml run conduktor apply -f /
-	docker compose -f docker/docker-compose.yml run conduktor delete -f /test_resource.yml
-	docker compose -f docker/docker-compose.yml run conduktor apply -f /
-	docker compose -f docker/docker-compose.yml run conduktor get Topic yolo --cluster=my-cluster
-	docker compose -f docker/docker-compose.yml run conduktor delete Topic yolo -v --cluster=my-cluster
-	docker compose -f docker/docker-compose.yml run -e CDK_USER=admin -e CDK_PASSWORD=secret conduktor login
-	docker compose -f docker/docker-compose.yml run -e CDK_USER=admin -e CDK_PASSWORD=secret  -e CDK_API_KEY="" conduktor get KafkaCluster my_kafka_cluster
-    docker compose -f docker/docker-compose.yml run conduktor token list admin
-	docker compose -f docker/docker-compose.yml run conduktor token list application-instance -i=my_app_instance
-	docker compose -f docker/docker-compose.yml run conduktor token create admin a_admin_token
-	docker compose -f docker/docker-compose.yml run conduktor token create application-instance -i=my_app_instance a_admin_token
-	docker compose -f docker/docker-compose.yml run conduktor token delete 0-0-0-0-0
+	docker compose -f docker/docker-compose.yml run --rm conduktor apply -f /test_resource.yml
+	docker compose -f docker/docker-compose.yml run --rm conduktor apply -f /
+	docker compose -f docker/docker-compose.yml run --rm conduktor delete -f /test_resource.yml
+	docker compose -f docker/docker-compose.yml run --rm conduktor apply -f /
+	docker compose -f docker/docker-compose.yml run --rm conduktor get Topic yolo --cluster=my-cluster
+	docker compose -f docker/docker-compose.yml run --rm conduktor delete Topic yolo -v --cluster=my-cluster
+	docker compose -f docker/docker-compose.yml run --rm -e CDK_USER=admin -e CDK_PASSWORD=secret conduktor login
+	docker compose -f docker/docker-compose.yml run --rm -e CDK_USER=admin -e CDK_PASSWORD=secret  -e CDK_API_KEY="" conduktor get KafkaCluster my_kafka_cluster
+  docker compose -f docker/docker-compose.yml run --rm conduktor token list admin
+	docker compose -f docker/docker-compose.yml run --rm conduktor token list application-instance -i=my_app_instance
+	docker compose -f docker/docker-compose.yml run --rm conduktor token create admin a_admin_token
+	docker compose -f docker/docker-compose.yml run --rm conduktor token create application-instance -i=my_app_instance a_admin_token
+	docker compose -f docker/docker-compose.yml run --rm conduktor token delete 0-0-0-0-0
+
+	# Gateway
+	docker compose -f docker/docker-compose.yml run --rm conduktor apply -f /test_resource_gw.yml
+	docker compose -f docker/docker-compose.yml run --rm conduktor delete VClusters vcluster1
+	docker compose -f docker/docker-compose.yml run --rm conduktor get VClusters vcluster1
 }
 
 main "$@"
