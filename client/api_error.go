f65bc029709c19daaaf4306c3572511ca3ce38a5
Ben Starmer-Smith <bstarmersmith@conduktor.io>
2025-03-13 16:51:17 +0000

    Now made the default error message string be the cause given by the API instead of the msg given.

--- client/api_error.go
+++ client/api_error.go
@@ -3,12 +3,15 @@ package client
 type ApiError struct {
 	Title string
 	Msg   string
+	Cause string
 }
 
 func (e *ApiError) String() string {
-	if e.Msg == "" {
-		return e.Title
-	} else {
+	if e.Cause != "" {
+		return e.Cause
+	} else if e.Msg != "" {
 		return e.Msg
+	} else {
+		return e.Title
 	}
 }
